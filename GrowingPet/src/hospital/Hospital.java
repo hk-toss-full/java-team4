369489a0429ae75cf5d73c79f65db4ac81b522d9
최창환 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-09-30 11:24:33 +0900

    refactor 병원 에러 메세지 출력

--- GrowingPet/src/hospital/Hospital.java
+++ GrowingPet/src/hospital/Hospital.java
@@ -10,11 +10,11 @@ import java.util.TimerTask;
 
 public class Hospital {
     private boolean isSick = false;
+    private static final String PET_DIE_ERROR_MESSAGE = "당신의 펫은 사망하였습니다.";
     Random random = new Random();
     Scanner scanner = new Scanner(System.in);
 
     public Hospital() {
-
         setTime();
     }
 
@@ -25,16 +25,20 @@ public class Hospital {
             public void run() {
                 isSick = true;
                 if (isSick = true) {
-                    System.out.println("당신의 펫 " + getPet() + " 병에 걸렸습니다.");
-                    System.out.println("당신의 펫 " + getPet() + " 치료하려면 0번, 치료하지 않으려면 1번을 입력하세요.");
+                    System.out.println("당신의 펫은 병에 걸렸습니다.");
+                    System.out.println("당신의 펫은 치료하려면 0번, 치료하지 않으려면 1번을 입력하세요.");
                     int number = scanner.nextInt();
-                    if (number == 1) {
+                    if (number == 0) {
                         getUser().setCoin(getUser().getCoin() - 300);
                         isSick = false;
+                    } else {
+                        System.out.println("당신의 펫은 치료받지 못해 죽었습니다.");
+
+                        throw new IllegalArgumentException(PET_DIE_ERROR_MESSAGE);
                     }
                 }
             }
         };
-        timer.schedule(task, time * 1000);
+        timer.schedule(task, time);
     }
 }
